36a201a2e9a9248364f85bd9db92f8e0311c58e8
Nuno Rodrigues <nunomr@gmail.com>
2017-07-16 19:10:23 +0100

    Allow to trigger the processors at different webpack stages / phases
    Currently the supported stages are:
    compilation.optimize-chunk-assets, compilation.optimize-assets, and emit.
    The emit stage is the default.
    Small refactor.

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 var assign = require('lodash/assign');
 var each = require('lodash/each');
+var find = require('lodash/find');
 var isArray = require('lodash/isArray');
 var isFunction = require('lodash/isFunction');
 var isRegExp = require('lodash/isRegExp');
 var keys = require('lodash/keys');
+var values = require('lodash/values');
 var webpackSources = require('webpack-sources');
 
-function emptyFunc() {};
+function EMPTY_FUNC() {};
+
+var PHASE = {
+  OPTIMIZE_CHUNK_ASSETS: 'compilation.optimize-chunk-assets',
+  OPTIMIZE_ASSETS: 'compilation.optimize-assets',
+  EMIT: 'emit'
+};
+var PHASES = values(PHASE);
 
 function LastCallWebpackPlugin(options) {
   this.options = assign(
     {
       assetProcessors: [],
-      onStart: emptyFunc,
-      onEnd: emptyFunc,
+      onStart: EMPTY_FUNC,
+      onEnd: EMPTY_FUNC,
       canPrint: true
     },
     options || {}
@@ -32,6 +41,12 @@ function LastCallWebpackPlugin(options) {
     }
     if (!isFunction(processor.processor)) {
       throw new Error('LastCallWebpackPlugin Error: invalid options.assetProcessors[' + String(index) + '].processor (must be a function).');
+    }
+    if (processor.phase === undefined) {
+      processor.phase = PHASE.EMIT;
+    }
+    if (!find(PHASES, function(p) { return p === processor.phase; })) {
+      throw new Error('LastCallWebpackPlugin Error: invalid options.assetProcessors[' + String(index) + '].phase (must be on of: ' + PHASES.join(', ') + ').');
     }
   });
 
@@ -53,23 +68,25 @@ LastCallWebpackPlugin.prototype.onAssetError = function(assetName, asset, err) {
   this.print('Error processing file: ' + assetName);
 };
 
-LastCallWebpackPlugin.prototype.getAssetsAndProcessors = function(assets) {
+LastCallWebpackPlugin.prototype.getAssetsAndProcessors = function(assets, phase) {
   var assetProcessors = this.options.assetProcessors;
   var assetNames = keys(assets);
   var assetsAndProcessors = [];
 
   each(assetNames, function (assetName) {
     each(assetProcessors, function(assetProcessor) {
-      var regExpResult = assetProcessor.regExp.exec(assetName);
-      assetProcessor.regExp.lastIndex = 0;
-      if (regExpResult) {
-        var assetAndProcessor = {
-          assetName: assetName,
-          regExp: assetProcessor.regExp,
-          processor: assetProcessor.processor,
-          regExpResult: regExpResult,
-        };
-        assetsAndProcessors.push(assetAndProcessor);
+      if (assetProcessor.phase === phase) {
+        var regExpResult = assetProcessor.regExp.exec(assetName);
+        assetProcessor.regExp.lastIndex = 0;
+        if (regExpResult) {
+          var assetAndProcessor = {
+            assetName: assetName,
+            regExp: assetProcessor.regExp,
+            processor: assetProcessor.processor,
+            regExpResult: regExpResult,
+          };
+          assetsAndProcessors.push(assetAndProcessor);
+        }
       }
     });
   });
@@ -81,45 +98,60 @@ LastCallWebpackPlugin.prototype.createAsset = function(content, originalAsset) {
   return new webpackSources.RawSource(content);
 };
 
+LastCallWebpackPlugin.prototype.process = function(compilation, phase, callback) {
+  var self = this;
+
+  var assetsAndProcessors = this.getAssetsAndProcessors(compilation.assets, phase);
+  if (assetsAndProcessors.length <= 0) {
+    return callback();
+  }
+
+  this.options.onStart(assetsAndProcessors, compilation, phase);
+
+  var hasErrors = false;
+  var promises = [];
+
+  each(assetsAndProcessors, function(assetAndProcessor) {
+    var asset = compilation.assets[assetAndProcessor.assetName];
+    var promise = assetAndProcessor
+      .processor(assetAndProcessor.assetName, asset)
+      .then(function (result) {
+        if (result !== undefined) {
+          compilation.assets[assetAndProcessor.assetName] = self.createAsset(result, asset);
+        }
+      })
+      .catch(function(err) {
+        hasErrors = true;
+        self.onAssetError(assetAndProcessor.assetName, asset, err);
+        throw err;
+      });
+    promises.push(promise);
+  });
+
+  return Promise.all(promises)
+    .then(function () {
+      self.options.onEnd(assetsAndProcessors, compilation, phase);
+      callback();
+    })
+    .catch(function (err) {
+      self.options.onEnd(assetsAndProcessors, compilation, phase, err);
+      callback(err);
+    });
+};
+
 LastCallWebpackPlugin.prototype.apply = function(compiler) {
   var self = this;
 
-  compiler.plugin('emit', function(compilation, compileCallback) {
-    var assets = compilation.assets;
-    var assetsAndProcessors = self.getAssetsAndProcessors(assets);
-
-    self.options.onStart(assets, assetsAndProcessors, compilation);
-
-    var hasErrors = false;
-    var promises = [];
-
-    each(assetsAndProcessors, function(assetAndProcessor) {
-      var asset = assets[assetAndProcessor.assetName];
-      var promise = assetAndProcessor.processor(assetAndProcessor.assetName, asset)
-        .then(function (result) {
-          if (!hasErrors) {
-            if (result !== undefined) {
-              assets[assetAndProcessor.assetName] = self.createAsset(result, asset);
-            }
-          }
-        })
-        .catch(function(err) {
-          hasErrors = true;
-          self.onAssetError(assetAndProcessor.assetName, asset, err);
-          throw err;
-        });
-      promises.push(promise);
+  compiler.plugin('compilation', function(compilation, params) {
+    compilation.plugin("optimize-chunk-assets", function(chunks, callback) {
+      self.process(compilation, PHASE.OPTIMIZE_CHUNK_ASSETS, callback);
     });
-
-    Promise.all(promises)
-      .then(function () {
-        self.options.onEnd();
-        compileCallback();
-      })
-      .catch(function (err) {
-        self.options.onEnd(err);
-        compileCallback(err);
-      });
+    compilation.plugin("optimize-assets", function(chunks, callback) {
+      self.process(compilation, PHASE.OPTIMIZE_ASSETS, callback);
+    });
+  });
+  compiler.plugin('emit', function(compilation, callback) {
+    self.process(compilation, PHASE.EMIT, callback);
   });
 };
 
